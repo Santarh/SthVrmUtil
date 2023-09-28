de0644a4c455d8d4756f4058f6c3b1158892c06c
Masataka SUMI <santarh@gmail.com>
2023-09-29 01:27:54 +0900

    適当に実装無クラス追加

---
+++ Packages/net.santarh.sth-vrm-util/Runtime/ExpressionWrapper/IVRMBlendShapeProxy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using VRM;
+
+namespace SthVrmUtil.Runtime.ExpressionWrapper
+{
+    public interface IVRMBlendShapeProxy
+    {
+        void Reinitialize();
+
+        // NOTE: Get
+        float GetValue(BlendShapeKey key);
+        float GetValue(BlendShapePreset key);
+        float GetValue(string key);
+        IEnumerable<KeyValuePair<BlendShapeKey, float>> GetValues();
+
+        // NOTE: 累積させてから反映
+        void AccumulateValue(BlendShapeKey key, float value);
+        void AccumulateValue(BlendShapePreset key, float value);
+        void AccumulateValue(string key, float value);
+        void Apply();
+
+        // NOTE: 引数によって即時反映かどうか変わる
+        void SetValue(BlendShapePreset key, float value, bool apply);
+        void SetValue(string key, float value, bool apply);
+        void SetValue(BlendShapeKey key, float value, bool apply);
+
+        // NOTE: 即時反映
+        void SetValue(BlendShapePreset key, float value);
+        void SetValue(string key, float value);
+        void SetValue(BlendShapeKey key, float value);
+        void SetValues(IEnumerable<KeyValuePair<BlendShapeKey, float>> values);
+        void ImmediatelySetValue(BlendShapeKey key, float value);
+        void ImmediatelySetValue(BlendShapePreset key, float value);
+        void ImmediatelySetValue(string key, float value);
+    }
+}
---
+++ Packages/net.santarh.sth-vrm-util/Runtime/ExpressionWrapper/SthVRMBlendShapeProxy.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using SthVrmUtil.Runtime.ExpressionWrapper;
+using UnityEngine;
+
+// NOTE: namespace は詐称する
+namespace VRM
+{
+    public class SthVRMBlendShapeProxy : MonoBehaviour, IVRMBlendShapeProxy
+    {
+        public void Reinitialize()
+        {
+            // NOTE: 再生成は VRM 1.0 モデルを色々弄る必要があってちょっとめんどい
+            throw new System.NotImplementedException();
+        }
+
+        public float GetValue(BlendShapeKey key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float GetValue(BlendShapePreset key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float GetValue(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<BlendShapeKey, float>> GetValues()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AccumulateValue(BlendShapeKey key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AccumulateValue(BlendShapePreset key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AccumulateValue(string key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Apply()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValue(BlendShapePreset key, float value, bool apply)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValue(string key, float value, bool apply)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValue(BlendShapeKey key, float value, bool apply)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValue(BlendShapePreset key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValue(string key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValue(BlendShapeKey key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetValues(IEnumerable<KeyValuePair<BlendShapeKey, float>> values)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ImmediatelySetValue(BlendShapeKey key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ImmediatelySetValue(BlendShapePreset key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ImmediatelySetValue(string key, float value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
